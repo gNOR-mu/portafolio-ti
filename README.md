169be8eac15fca3f92d8888c7bc0ad0dfb826d8c
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-03-29 00:43:54 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# portafolio-ti
+
+Link del código portafolio: https://github.com/gNOR-mu/portafolio-ti
+
+# Tecnologías utilizadas
+
+- Bootstrap
+- jQuery
+- Vanta.js (animaciones)
---
+++ assets/js/vantaConfig.js
@@ -0,0 +1,14 @@
+VANTA.GLOBE({
+    el: "#inicio",
+    mouseControls: true,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x968039,
+    color2: 0xffffff,
+    backgroundColor: 0x0d1117,
+
+})
